0f19ec457fea8c83da10068f72a9daf78d8d0a18
YPAzevedo <yago.pereira.az@gmail.com>
2020-04-12 00:28:50 -0300

    desafio

--- src/index.js
+++ src/index.js
@@ -1,133 +1,89 @@
-const express = require('express');
-const {uuid, isUuid} = require('uuidv4');
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-/**
- * HTTP Methods:
- * 
- * GET: get information from the backend.
- * POST: create something on the backend.
- * PUT/PATCH: modify something on the backend.
- * DELETE: delete something on the backend.
- */
+const repositories = [];
 
-  /**
-  * Param types:
-  * 
-  * Query params: filters and pagiantion.
-  * Route params: identify resources (modify/create).
-  * Body params: content to create or modify.
-  */
+app.get("/repositories", (req, res) => {
+  return res.status(200).json(repositories);
+});
 
-  /**
-  * Middleware: intercepts requests. 
-  * It can stop requests or modify data on the request.
-  */
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body;
 
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
 
-const projects = [];
+  repositories.push(repository);
 
-const logRequests = (req, res, next) => {
-  const { method, url } = req;
+  return res.status(201).json(repository);
+});
 
-  const logLabel = `[${method.toUpperCase()} ${url}]`;
-
-  console.time(logLabel);
-
-  next(); //Call the route you requested.
-
-  console.timeEnd(logLabel);// Called after you call the route.
-}
-
-const validateProjectId = (req, res, next) => {
+app.put("/repositories/:id", (req, res) => {
+  const { title, url, techs } = req.body;
   const { id } = req.params;
 
-  if(!isUuid(id)){
-    return status(400).json({
-      error: "Not a valid ID."
-    })
-  }
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
 
-  return next()
-}
-
-app.use(logRequests);
-app.use('/projects/:id', validateProjectId);
-
-app.get('/projects', (req, res) => {
-  const { search } = req.query;
-
-  const result = search ?
-    projects.filter(item => item.project.toLowerCase().includes(search.toLowerCase())) 
-    : 
-    projects;
-
-  return res.json(result);
-});
-
-app.get('/projects/:id', (req, res) => {
-  const { params } = req;
-
-  if(params.id > projects.length) {
-    return res.status(404).json({
-      error: 'Project not found.'
+  if (repoIndex === -1) {
+    return res.status(400).json({
+      error: true,
+      message: 'Repository was not found',
     });
-  } ;
-
-  return res.json(projects[params.id]);
-})
-
-app.post('/projects', (req, res) => {
-  const { project } = req.body
-
-  const newProject = { id: uuid(), project }
-
-  projects.push(newProject);
-
-  return res.json(projects)
-})
-
-app.put('/projects/:id', (req, res) => {
-  const { body, params } = req;
-
-  const projectIndex = projects.findIndex(item => item.id === params.id)
-
-  if(projectIndex < 0) {
-    return res.status(400).json({
-      error: 'No project found.'
-    })
   }
 
-  const project = {
-    id: params.id,
-    project: body.project,
-  }
-  
-  projects[projectIndex] = project;
+  const repository = {
+    title,
+    url,
+    techs,
+  };
 
-  return res.json(projects);
-})
+  Object.assign(repositories[repoIndex], repository);
 
-app.delete('/projects/:id', (req, res) => {
-  const { params } = req
-
-  const projectIndex = projects.findIndex(item => item.id === params.id)
-
-  if(projectIndex < 0) {
-    return res.status(400).json({
-      error: 'No project found.'
-    })
-  }
-
-  projects.splice(projectIndex, 1);
-  
-  return res.json(projects);
-})
-
-app.listen(3333, () => {
-  console.log('💯- Backend started!');
+  return res.status(200).json(repositories[repoIndex]);
 });
 
+app.delete("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+
+  const repoIndex =  repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return res.status(400).json({
+      error: 'Repository not found',
+    });
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return res.status(204).send();
+});
+
+app.post("/repositories/:id/like", (req, res) => {
+  const { id } = req.params;
+
+  const repoIndex =  repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return res.status(400).json({
+      error: 'Repository not found',
+    });
+  }
+
+  Object.assign(repositories[repoIndex], { likes: repositories[repoIndex].likes + 1 });
+
+  return res.status(200).json(repositories[repoIndex]);
+});
+
+module.exports = app;
